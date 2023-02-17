24b447a7a42ed47bba246334c956b0a66c54679c
AlexPresti2021 <93148794+AlexPresti2021@users.noreply.github.com>
2023-02-17 14:25:07 +0100

    b

--- 2.Llenguatge de progrmació Arduino/2.2 Tipus de dades i operadors/Ex 10/Ex 10.ino
+++ 2.Llenguatge de progrmació Arduino/2.2 Tipus de dades i operadors/Ex 10/Ex 10.ino
@@ -8,12 +8,19 @@
 //************************ INCLUDE ******************************
 
 //*********************** VARIABLES *****************************
-
+int test = 32767;
 
 //************************* SETUP *******************************
 
 void setup() {               // configura el final de salida
+  Serial.begin(9600);     //set up Serial library at 9600 bps
+  Serial.print("Test value is: ");
+  Serial.println(test);
+
+  test = test + 1;
  
+  Serial.print("Now it is: ");
+  Serial.println(test);
 
 
 }
--- 2.Llenguatge de progrmació Arduino/2.2 Tipus de dades i operadors/Ex 6/Ex 6.ino
+++ 2.Llenguatge de progrmació Arduino/2.2 Tipus de dades i operadors/Ex 6/Ex 6.ino
@@ -8,14 +8,22 @@
 //************************ INCLUDE ******************************
 
 //*********************** VARIABLES *****************************
-
+int drive_gb = 5;
+int drive_mb = 0;
 
 //************************* SETUP *******************************
 
 void setup() {               // configura el final de salida
- 
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large.");
 
+  drive_mb = 1024 * drive_gb;
 
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
 }
 
 //*************************** LOOP ******************************
